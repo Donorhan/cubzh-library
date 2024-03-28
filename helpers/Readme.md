ca4d07b8b1310f549bd3b0c9d30e28bd0052b137
Dono <3781087+Donorhan@users.noreply.github.com>
2024-03-28 18:29:05 +0100

    helpers: add new methods

---
+++ helpers/Readme.md
@@ -0,0 +1,57 @@
+## Installation
+```
+Modules = {
+  helpers = "github.com/Donorhan/cubzh-library/helpers:[commit-id]",
+}
+```
+
+## How to
+### Table
+`helpers.table.display(table, [customDisplay])`
+Displays key-value pairs of the given map on the console.
+
+`helpers.table.every(table, callback)`
+Checks if a callback function returns true for every element in the table.
+
+`helpers.table.fill(table, value)`
+Fills the table with the value given as a parameter
+
+`helpers.table.filter(table, callback)`
+Creates a new table containing only the elements from the original table for which the callback function returns true.
+
+`helpers.table.forEach(table, callback)`
+Executes a callback function for each element in the table.
+
+`helpers.table.get(table, key, default)`
+Retrieves an element from the array; if the value does not exist, returns the default value given in parameter
+
+`helpers.table.keys(table)`
+Retrieves all the keys of a table
+
+`helpers.table.length(table)`
+Returns the number of elements in the table.
+
+`helpers.table.map(table, callback)`
+Modifies each element in the table using the provided callback function.
+
+`helpers.table.merge(firstTable, secondTable)`
+Merges two arrays: if an element in the second array already exists in the first, its value will be overwritten.
+
+`helpers.table.sample(table)`
+Take a random element from the array.
+
+`helpers.table.values(table)`
+Returns an array containing all the values from the table.
+
+`helpers.table.removeKey(table, key)`
+Removes a key-value pair from the table, if the key exists.
+
+`helpers.table.some(table, callback)`
+Checks if at least one element in the table satisfies the condition specified by the callback function.
+
+### Math
+`helpers.math.remap(value, low1, high1, low2, high2)`
+Remaps a value from one range to another using linear interpolation.
+
+`helpers.math.round(value)`
+Round a number to its nearest higher or lower value.
--- helpers/helpers.lua
+++ helpers/helpers.lua
@@ -1,83 +1,144 @@
-local helpers = {}
+local helpers = {
+    table = {},
+    math = {},
+}
 
-helpers.display = function(map)
-  for index, value in pairs(map) do
-    print(index, " -> ", value)
-  end
-end
-
-helpers.every = function(t, callback)
-  local mapLength = helpers.length(t)
-  local succeedCount = 0
-
-  for index, value in pairs(t) do
-    if callback(value, index, t) then
-      succeedCount = succeedCount + 1
+helpers.table.display = function(map, customDisplay)
+    if not customDisplay then
+        for index, value in pairs(map) do
+            print(index, " -> ", value)
+        end
+    else
+        for index, value in pairs(map) do
+            print(customDisplay(value, index, map))
+        end
     end
-  end
-
-  return succeedCount == mapLength
 end
 
-helpers.filter = function(t, callback)
-  local filteredMap = {}
-  for key, value in pairs(t) do
-    if callback(value, key, t) then
-      filteredMap[key] = value
+helpers.table.every = function(t, callback)
+    local mapLength = helpers.table.length(t)
+    local succeedCount = 0
+
+    for index, value in pairs(t) do
+        if callback(value, index, t) then
+            succeedCount = succeedCount + 1
+        end
     end
-  end
 
-  return filteredMap
+    return succeedCount == mapLength
 end
 
-helpers.forEach = function(t, callback)
-  for index, value in pairs(t) do
-    callback(value, index, t)
-  end
-end
-
-helpers.length = function(t)
-  local count = 0
-  for _ in pairs(t) do
-    count = count + 1
-  end
-
-  return count
-end
-
-helpers.map = function(t, callback)
-  for index, value in pairs(t) do
-    t[index] = callback(value, index, t)
-  end
-end
-
-helpers.values = function(t)
-  local values = {}
-  for _, value in pairs(t) do
-    table.insert(values, value)
-  end
-
-  return values
-end
-
-helpers.removeKey = function(t, key)
-  if t[key] then
-    t[key] = nil
-  end
-end
-
-helpers.some = function(t, callback)
-  for index, value in pairs(t) do
-    if callback(value, index, t) then
-      return true
+helpers.table.fill = function(t, value)
+    for index, _ in pairs(t) do
+        t[index] = value
     end
-  end
-
-  return false
 end
 
-helpers.remap = function(value, low1, high1, low2, high2)
-  return low2 + (value - low1) * (high2 - low2) / (high1 - low1)
+helpers.table.filter = function(t, callback)
+    local filteredMap = {}
+    for key, value in pairs(t) do
+        if callback(value, key, t) then
+            filteredMap[key] = value
+        end
+    end
+
+    return filteredMap
+end
+
+helpers.table.findIndex = function(t, value)
+    for index, tableValue in pairs(t) do
+        if tableValue == value then
+            return index
+        end
+    end
+
+    return -1
+end
+
+helpers.table.forEach = function(t, callback)
+    for index, value in pairs(t) do
+        callback(value, index, t)
+    end
+end
+
+helpers.table.get = function(t, key, default)
+    return t[key] or default
+end
+
+helpers.table.keys = function(t)
+    local keys = {}
+    for key in pairs(t) do
+        table.insert(keys, key)
+    end
+
+    return keys
+end
+
+helpers.table.length = function(t)
+    local count = 0
+    for _ in pairs(t) do
+        count = count + 1
+    end
+
+    return count
+end
+
+helpers.table.map = function(t, callback)
+    for index, value in pairs(t) do
+        t[index] = callback(value, index, t)
+    end
+end
+
+helpers.table.merge = function(firstTable, secondTable)
+    local mergedTables = {}
+
+    local func = function(value, key) mergedTables[key] = value end
+    helpers.table.forEach(firstTable, func)
+    helpers.table.forEach(secondTable, func)
+
+    return mergedTables
+end
+
+helpers.table.sample = function(t)
+    local keys = helpers.table.keys(t)
+    local size = #keys
+    if size > 0 then
+        local key = keys[math.random(size)]
+        return t[key]
+    end
+end
+
+helpers.table.values = function(t)
+    local values = {}
+    for _, value in pairs(t) do
+        table.insert(values, value)
+    end
+
+    return values
+end
+
+helpers.table.removeKey = function(t, key)
+    if t[key] then
+        t[key] = nil
+    end
+end
+
+helpers.table.some = function(t, callback)
+    for index, value in pairs(t) do
+        if callback(value, index, t) then
+            return true
+        end
+    end
+
+    return false
+end
+
+helpers.math.remap = function(value, low1, high1, low2, high2)
+    return low2 + (value - low1) * (high2 - low2) / (high1 - low1)
+end
+
+helpers.math.round = function(value)
+    return math.floor(value + 0.5)
 end
 
 return helpers
