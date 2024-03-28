a2c9886f0681ea85e81a8cc6315b562cd032dbc9
Dono <3781087+Donorhan@users.noreply.github.com>
2024-03-28 18:29:50 +0100

    add tagger module

---
+++ tagger/Readme.md
@@ -0,0 +1,44 @@
+## Installation
+```
+Modules = {
+  helpers = "github.com/Donorhan/cubzh-library/tagger:[commit-id]",
+}
+```
+
+## How to
+### Functions
+`tagger.tag(object, tags: array)`
+Associates one or more tags with a given object.
+
+`tagger.findAll(tags: array)`
+Retrieves all objects that have been tagged with any of the specified tags. It returns an array containing all the objects found.
+
+`tagger.findAllWithAllTags(tags: array)`
+Return all objects possessing the entire set of tags provided as parameters.
+
+`tagger.call(tags, callback)`
+Finds all objects associated with the specified tags and executes a callback function on each of them.
+
+`tagger.removeTags(object, [tags: array])`
+Removes tags from an object. Deletes all tags if the "tags" parameter is not specified
+
+## Examples
+```
+local playerA = { ["name"] = "Foo" }
+local playerB = { ["name"] = "Bar" }
+local playerC = { ["name"] = "Baz" }
+
+tagger.tag(playerA, { "friends" })
+tagger.tag(playerB, { "enemies" })
+tagger.tag(playerC, { "friends", "npc" })
+
+local friendsWithTags = tagger.findAll({ "enemies", "npc" })
+table.display(friendsWithAllTags) -- Bar, Baz
+
+local friendsWithAllTags = tagger.findAllWithAllTags({ "friends", "npc" })
+table.display(friendsWithAllTags) -- Baz
+
+tagger.call({ "enemies", "npc" }, function(object)
+    -- Bar, Baz
+end)
+```
---
+++ tagger/tagger.lua
@@ -0,0 +1,95 @@
+local tagger = {}
+tagger.tags = {}
+
+local findTableIndex = function(t, v)
+    for index, tableValue in pairs(t) do
+        if tableValue == v then
+            return index
+        end
+    end
+
+    return -1
+end
+
+tagger.tag = function(object, tags)
+    for _, tag in ipairs(tags) do
+        if not tagger.tags[tag] then
+            tagger.tags[tag] = {}
+        end
+
+        table.insert(tagger.tags[tag], object)
+    end
+end
+
+tagger.findAll = function(tags)
+    local objects = {}
+    for _, tag in ipairs(tags) do
+        if tagger.tags[tag] then
+            for _, object in ipairs(tagger.tags[tag]) do
+                table.insert(objects, object)
+            end
+        end
+    end
+
+    return objects
+end
+
+tagger.findAllWithAllTags = function(tags)
+    local objects = {}
+    local firstTag = tags[1]
+
+    if not tagger.tags[firstTag] then
+        return objects
+    end
+
+    local taggedObjects = {}
+    for _, object in ipairs(tagger.tags[firstTag]) do
+        taggedObjects[object] = true
+    end
+
+    for i = 2, #tags do
+        local tag = tags[i]
+        if not tagger.tags[tag] then
+            return objects
+        end
+
+        for object, _ in pairs(taggedObjects) do
+            if findTableIndex(tagger.tags[tag], object) == -1 then
+                taggedObjects[object] = nil
+            end
+        end
+    end
+
+    for object, _ in pairs(taggedObjects) do
+        table.insert(objects, object)
+    end
+
+    return objects
+end
+
+tagger.call = function(tags, callback)
+    local objects = tagger.findAll(tags)
+    for _, object in ipairs(objects) do
+        callback(object)
+    end
+end
+
+tagger.removeTags = function(object, tags)
+    if tags then
+        for i, tag in ipairs(tags) do
+            local index = findTableIndex(tagger.tags[tag], object)
+            if index ~= -1 then
+                table.remove(tagger.tags[tag], index)
+            end
+        end
+    else
+        for tag, objects in pairs(tagger.tags) do
+            local index = findTableIndex(objects, object)
+            if index ~= -1 then
+                table.remove(tagger.tags[tag], index)
+            end
+        end
+    end
+end
+
+return tagger
